0835a2cc6e399c8681742de4b14c4b9ed657f4b9
eightXi <tuhaotian7@gmail.com>
2021-09-26 17:20:08 +0800

    fix: template更新

--- src/index.js
+++ src/index.js
@@ -5,32 +5,31 @@ import fs from '#/shaders/fs.glsl';
 var cvs = document.querySelector("#glcanvas");
 var gl = cvs.getContext('webgl') || cvs.getContext('experimental-webgl');
 
-function loadShader(gl, type, source) {
-    // 创建着色器对象
-    const shader = gl.createShader(type);
-    // 向着色器对象中填充着色器
-    gl.shaderSource(shader, source);
-    // 编译着色器
-    gl.compileShader(shader);
+var program = createProgram(gl, vs, fs); // 创建程序对象
 
-    return shader;
-}
+var a_Position = gl.getAttribLocation(program, 'a_Position') //获取attribute变量的存储位置
+gl.vertexAttrib3f(a_Position, 0.0, 0.0, 0.0); //向attribute变量赋值
 
-function initShaders(gl, vs, fs){
-    var program = gl.createProgram(); // 创建程序对象
-
-    const vertexShader = loadShader(gl, gl.VERTEX_SHADER, vs); // 创建顶点着色器对象
-    const fragmentShader = loadShader(gl, gl.FRAGMENT_SHADER, fs);
-
-    gl.attachShader(program, vertexShader); // 为程序对象分配着色器
-    gl.attachShader(program, fragmentShader);
-
-    gl.linkProgram(program); //链接程序对象
-    gl.useProgram(program); // 使用程序对象
-}
-
-initShaders(gl, vs, fs)
 
 gl.clearColor(0.0, 0.0, 0.0, 1.0);
 gl.clear(gl.COLOR_BUFFER_BIT);
 gl.drawArrays(gl.POINTS, 0, 1);
+
+function createProgram(gl, vs, fs) {
+    var program = gl.createProgram(); //创建程序对象
+    var vShader = createShader(gl, vs, gl.VERTEX_SHADER); 
+    var fShader = createShader(gl, fs, gl.FRAGMENT_SHADER);
+    gl.linkProgram(program); //链接程序对象
+    gl.useProgram(program); //使用程序对象
+
+    return program;
+
+    /**创建着色器 */
+    function createShader(webgl, source, type) {
+        const shader = webgl.createShader(type); // 创建着色器对象
+        webgl.shaderSource(shader, source); //填充着色器
+        webgl.compileShader(shader); // 编译着色器
+        webgl.attachShader(program, shader); //为程序对象分配着色器
+        return shader;
+    }
+} 
