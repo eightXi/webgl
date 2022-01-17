46e14e64ab8b590ce2aeebc52a59bce988d78678
eightXi <tuhaotian7@gmail.com>
2022-01-17 11:49:41 +0800

    feat: chapter3

--- src/pages/chapter3/index.js
+++ src/pages/chapter3/index.js
@@ -11,6 +11,7 @@ const programInfo = {
     program,
     attribLocations: {
       vertexPosition: gl.getAttribLocation(program, 'aVertexPosition'),
+      vertexColor: gl.getAttribLocation(program, 'aVertexColor')
     },
     uniformLocations: {
       projectionMatrix: gl.getUniformLocation(program, 'uProjectionMatrix'),
@@ -23,8 +24,6 @@ drawScene(gl, programInfo, buffers);
 
 function initBuffers(gl) {
     const positionBuffer = gl.createBuffer();
-    gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
-
     var vertices = [
         1.0,  1.0,  0.0,
         -1.0, 1.0,  0.0,
@@ -32,34 +31,31 @@ function initBuffers(gl) {
         -1.0, -1.0, 0.0
     ];
 
+    gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
     gl.bufferData(gl.ARRAY_BUFFER,
                     new Float32Array(vertices),
                     gl.STATIC_DRAW); // static_draw指定数据存储区的使用方法 
                                     // 缓冲区的内容可能经常使用，而不会经常更改。
 
+    const colorBuffer = gl.createBuffer();
+    const fragements = [
+        1.0,  1.0,  1.0,  1.0,    // 白
+        1.0,  0.0,  0.0,  1.0,    // 红
+        0.0,  1.0,  0.0,  1.0,    // 绿
+        0.0,  0.0,  1.0,  1.0,    // 蓝
+    ];
+
+    gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER,
+        new Float32Array(fragements),
+        gl.STATIC_DRAW
+    );
+
     return {
-        position: positionBuffer
+        position: positionBuffer,
+        color: colorBuffer
     };
 }
-
-// 创建着色器
-function createShader(webgl, source, type) { // 渲染上下文，数据源，着色器类型
-    const shader = webgl.createShader(type); // 创建着色器对象
-    webgl.shaderSource(shader, source); //填充着色器
-    webgl.compileShader(shader); // 编译着色器
-    return shader;
-}
-// 创建着色程序
-function createProgram(gl, vs, fs) {
-    var program = gl.createProgram(); //创建程序对象
-    var vShader = createShader(gl, vs, gl.VERTEX_SHADER); 
-    var fShader = createShader(gl, fs, gl.FRAGMENT_SHADER);
-    gl.attachShader(program, vShader); //为程序对象分配着色器
-    gl.attachShader(program, fShader);
-    gl.linkProgram(program); //链接程序对象
-
-    return program;
-} 
 
 function drawScene(gl, programInfo, buffers) {
     gl.clearColor(0.0, 0.0, 0.0, 1.0);  // 清除背景色为黑色
@@ -94,7 +90,7 @@ function drawScene(gl, programInfo, buffers) {
   
     mat4.translate(modelViewMatrix,     // 用给定的向量平移
                    modelViewMatrix,     
-                   [-0.0, 0.0, -4.0]);  // 距离相机6个单位
+                   [-0.0, 0.0, -4.0]);  // 距离相机4个单位
   
     // 从位置缓冲区中提取位置到vertexPosition属性中。
     {
@@ -105,7 +101,7 @@ function drawScene(gl, programInfo, buffers) {
                                 // 0 = use type and numComponents above
       const offset = 0;         // 从缓冲起始位置开始读取
       gl.bindBuffer(gl.ARRAY_BUFFER, buffers.position);
-      gl.vertexAttribPointer(
+      gl.vertexAttribPointer(  // 告诉显卡从当前绑定的缓冲区（bindBuffer()指定的缓冲区）中读取顶点数据
           programInfo.attribLocations.vertexPosition,
           numComponents,
           type,
@@ -115,6 +111,25 @@ function drawScene(gl, programInfo, buffers) {
       gl.enableVertexAttribArray(  // 打开属性数组列表中指定索引处的通用顶点属性数组
           programInfo.attribLocations.vertexPosition);
     }
+
+    {
+        const numComponents = 4;  // 每次迭代提取4个值
+        const type = gl.FLOAT;    // 32位浮点数
+        const normalize = false;  // 不需要归一化数据
+        const stride = 0;         // 每次迭代运行运动多少内存到下一个数据开始点
+                                  // 0 = use type and numComponents above
+        const offset = 0;         // 从缓冲起始位置开始读取
+        gl.bindBuffer(gl.ARRAY_BUFFER, buffers.color);
+        gl.vertexAttribPointer(
+            programInfo.attribLocations.vertexColor,
+            numComponents,
+            type,
+            normalize,
+            stride,
+            offset);
+        gl.enableVertexAttribArray(  // 打开属性数组列表中指定索引处的通用顶点属性数组
+            programInfo.attribLocations.vertexColor);
+      }
 
     gl.useProgram(programInfo.program);
   
@@ -135,4 +150,22 @@ function drawScene(gl, programInfo, buffers) {
       gl.drawArrays(gl.TRIANGLE_STRIP, offset, vertexCount);  // 三角带，需要控制点位置顺序
     }
   }
-  
+
+// 创建着色器
+function createShader(webgl, source, type) { // 渲染上下文，数据源，着色器类型
+    const shader = webgl.createShader(type); // 创建着色器对象
+    webgl.shaderSource(shader, source); //填充着色器
+    webgl.compileShader(shader); // 编译着色器
+    return shader;
+}
+// 创建着色程序
+function createProgram(gl, vs, fs) {
+    var program = gl.createProgram(); //创建程序对象
+    var vShader = createShader(gl, vs, gl.VERTEX_SHADER); 
+    var fShader = createShader(gl, fs, gl.FRAGMENT_SHADER);
+    gl.attachShader(program, vShader); //为程序对象分配着色器
+    gl.attachShader(program, fShader);
+    gl.linkProgram(program); //链接程序对象
+
+    return program;
+} 
--- src/pages/chapter3/shaders/fs.glsl
+++ src/pages/chapter3/shaders/fs.glsl
@@ -1,5 +1,7 @@
 precision mediump float;// 片元着色器需要设置精度
 
+varying lowp vec4 vColor;
+
 void main(){
-    gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);
+    gl_FragColor = vColor;
 }
